d9783e0d6780d975f8f0948142a15abf6a038bcd
aiyaz.parmar <aiyaz.parmar@petpooja.com>
2022-02-17 21:49:33 +0530

    - Changes

--- app/src/main/java/com/aiyaz/calculatormvvm/ViewModelMain.kt
+++ app/src/main/java/com/aiyaz/calculatormvvm/ViewModelMain.kt
@@ -6,49 +6,57 @@ import androidx.lifecycle.ViewModel
 
 class ViewModelMain : ViewModel() {
     private var totalValueMutable = MutableLiveData<String>()
-    val totalValue : LiveData<String>
-    get() = totalValueMutable
+    val totalValue: LiveData<String>
+        get() = totalValueMutable
 
     var calcOperations = CalcOperations.NotDefined
 
     private var firstCalcArg = 0.0
     private var secondCalcArg = 0.0
 
-    fun setCalcStr(passedVal : String){
-        val passedValueInt = passedVal.toIntOrNull()?:-1
-        if(passedValueInt > -1){
-            if(totalValueMutable.value == null){
-                totalValueMutable.value = passedVal
-            }else {
+    fun setCalcStr(passedVal: String) {
+        if(calcOperations != CalcOperations.NotDefined){
+            totalValueMutable.value = ""
+        }
+        val passedValueInt = passedVal.toIntOrNull() ?: -1
+        if (passedVal == "00") {
+            if (totalValueMutable.value.toString().isEmpty()) {
+                totalValueMutable.value = ""
+            } else {
                 totalValueMutable.value = "${totalValueMutable.value}${passedVal}"
             }
-        }else{
+        } else if (passedValueInt > -1) {
+            if (totalValueMutable.value == null) {
+                totalValueMutable.value = passedVal
+            } else {
+                totalValueMutable.value = "${totalValueMutable.value}${passedVal}"
+            }
+        } else {
 
         }
     }
 
-    private fun clearCalcStr(){
+    private fun clearCalcStr() {
         totalValueMutable.value = ""
     }
 
-    fun clearVariablesAndStr(){
+    fun clearVariablesAndStr() {
         totalValueMutable.value = ""
         firstCalcArg = 0.0
         secondCalcArg = 0.0
     }
 
-    fun setOperation(calcOperations : CalcOperations){
+    fun setOperation(calcOperations: CalcOperations) {
         this.calcOperations = calcOperations
-        firstCalcArg = totalValueMutable.value?.toDoubleOrNull()?:0.0
-        clearCalcStr()
+        firstCalcArg = totalValueMutable.value?.toDoubleOrNull() ?: 0.0
     }
 
-    fun onEqualClickOperation(){
-        secondCalcArg = totalValueMutable.value?.toDoubleOrNull()?:0.0
+    fun onEqualClickOperation() {
+        secondCalcArg = totalValueMutable.value?.toDoubleOrNull() ?: 0.0
         calculateOperations()
     }
 
-    private fun calculateOperations(){
+    private fun calculateOperations() {
         when (calcOperations) {
             CalcOperations.Plus -> {
                 totalValueMutable.value = firstCalcArg.plus(secondCalcArg).toString()
@@ -62,8 +70,19 @@ class ViewModelMain : ViewModel() {
             CalcOperations.Divide -> {
                 totalValueMutable.value = firstCalcArg.div(secondCalcArg).toString()
             }
+            CalcOperations.Modulo -> {
+                totalValueMutable.value = firstCalcArg.mod(secondCalcArg).toString()
+            }
+            CalcOperations.NotDefined -> {
+                clearVariablesAndStr()
+            }
         }
     }
 
+
+    fun backSpace() {
+        totalValueMutable?.value = totalValueMutable.value.toString()
+            .substring(0, totalValueMutable.value.toString().length - 1)
+    }
 
 }
